71eadee0168bc793d594fbd9a49eeceec5d3449c
98Anderson98 <143131133+98Anderson98@users.noreply.github.com>
2025-05-03 23:27:46 -0500

    Add files via upload

---
+++ sketch_apr25a.ino
@@ -0,0 +1,29 @@
+  const int s0 = 4;
+  const int s1 = 5;
+  const int s2 = 6;
+  const int s3 = 7;
+  const int out = 8;
+
+
+void setup() {
+ serial.begin(9600);
+ pinMode(s0,OUTPUT);
+ pinMode(s1,OUTPUT);
+ pinMode(s2,OUTPUT);
+ pinMode(s3,OUTPUT);
+ pinMode(s4,OUTPUT);
+
+ digitalWrite(s0,HIGH);
+ gifitalWrite(s1, HIGH);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+int getRojo({
+  digitalWrite(s#,Low
+})
